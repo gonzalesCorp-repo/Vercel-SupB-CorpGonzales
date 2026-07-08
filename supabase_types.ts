ed9ce3d36732cabf8516cf12ed8eba9819b32c90
Cristian <crial0810@gmail.com>
2026-07-07 19:28:35 -0500

    fix: remove temp files causing build error

--- supabase_types.ts
+++
@@ -1,1 +0,0 @@
-{"_tag":"Error","error":{"code":"LegacyPlatformAuthRequiredError","message":"Access token not provided. Supply an access token by running `supabase login` or setting the SUPABASE_ACCESS_TOKEN environment variable."}}
