fc4ae614d3798967a37f0e9a752588c48db7f6d4
Cristian <crial0810@gmail.com>
2026-08-04 15:47:45 -0500

    fix: Desplazar posicionamiento de widget flotante de alertas a la izquierda del boton V.AI para eliminar la superposicion

--- src/components/recepcion/ActiveOATCsTable.tsx
+++ src/components/recepcion/ActiveOATCsTable.tsx
@@ -199,7 +199,7 @@ export default function ActiveOATCsTable({ onGenerarOrden }: ActiveOATCsTablePro
 
       {/* FLOATING ALERTS WIDGET */}
       {pendingAlerts.length > 0 && (
-        <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end">
+        <div className="fixed bottom-6 right-24 md:right-28 z-50 flex flex-col items-end">
           
           {/* Minimized Badge */}
           {isAlertsMinimized ? (
